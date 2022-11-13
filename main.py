ae3c5e11a58d7c46bd1ac7f8ffd5f6b8c89b62a6
N-a290 <nietoangel2902@gmail.com>
2022-11-13 16:59:25 -0500

    Cambiando el mensaje

--- main.py
+++ main.py
@@ -2,17 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI ()
 
-@app.get ("/saludo")
+@app.get ("/")
 async def root():
 
-    return {"message": "Hola MisionTIC 2022"}
-
-@app.get ("/usuarios/{user_id}")
-async def user(user_id : int):
-    return {"user_id": user_id}
-
-courses_list = [{"nombre": "Fundamentos de Programacion"}, {"nombre": "Programacion basica"}, {"nombre": "Desarrollo de Software"}, {"nombre": "Desarrollo de Aplicaciones Web"}]
-
-@app.get ("/cursos/")
-async def read_item(skip: int=0 , limit: int=10):
-    return courses_list[skip: skip+limit]
+    return {"message": "API de Gestion Documental"}
