39f25cf687248b0e1bc8a958609deae2dfef54f4
N-a290 <nietoangel2902@gmail.com>
2022-11-28 23:55:08 -0500

    Backend para usuarios terminado

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from routes.document import document_router
+from routes.user import user_router
 
 app = FastAPI ()
 
 app.include_router(document_router)
+app.include_router(user_router)
+
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,30 +1,42 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from config.db import client
 from schemas.user import userEntity, usersEntity
 from models.user import User
+from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 
-user = APIRouter()
+user_router = APIRouter()
 
-@user.get('/user')
+@user_router.get('/user', response_model=list[User], tags=["Usuarios"])
 def find_all_user():
     return usersEntity(client.Misiontic.User.find()) 
 
-@user.post('/user')
+@user_router.post('/user', response_model=User, tags=["Usuarios"])
 def create_user(user: User):
     new_user = dict(user)
+    new_user['password'] = sha256_crypt.encrypt(new_user['password'])
 
     id = client.Misiontic.User.insert_one(new_user).inserted_id
+    selected_user = client.Misiontic.User.find_one({"_id": id})
 
-    return str(id)
+    return userEntity(selected_user)
 
-@user.get('/user/{id}')
-def find_user():
-    return 'helloworld'
+@user_router.get('/user/{id}', response_model=User, tags=["Usuarios"])
+def find_user(id: str):
+    return userEntity(client.Misiontic.User.find_one({"_id": ObjectId(id)}))
 
-@user.put('/user/{id}')
-def edit_user():
-    return 'helloworld'
+@user_router.put('/user/{id}', response_model=User, tags=["Usuarios"])
+def update_user(id: str, user: User):
+    new_user = dict(user)
+    new_user['password'] = sha256_crypt.encrypt(new_user['password'])
+    
+    client.Misiontic.User.find_one_and_update({"_id": ObjectId(id)}, {'$set': dict(new_user)})
+    updated_user = client.Misiontic.User.find_one({"_id": ObjectId(id)})
+    
+    return userEntity(updated_user)
 
-@user.delete('/user/{id}')
-def delete_user():
-    return 'helloworld'
+@user_router.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["Usuarios"])
+def delete_user(id: str):
+    userEntity(client.Misiontic.User.find_one_and_delete({"_id": ObjectId(id)}))
+    return Response(status_code=HTTP_204_NO_CONTENT)
