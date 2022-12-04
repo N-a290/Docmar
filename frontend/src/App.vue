b791e19ad9e955698c4aca71ea0007c3ec6bec0a
N-a290 <nietoangel2902@gmail.com>
2022-12-04 14:09:28 -0500

    Implemented Vite + Vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/contact">Contacto</router-link>
+
+    <router-view />
+    <p>Sacado de este tutorial https://www.vuemastery.com/blog/vue-router-a-tutorial-for-vue-3/#starting-from-scratch
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
